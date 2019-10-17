548fdeef38bbbfcfa79e475f059a1eaa1d635bbf
Vamshi Krishna Kancharla <vamshi.k.kancharla@gmail.com>
2019-10-17 19:54:29 +0530

    DesignYourLife: WebService: Parsing Image File contents into Sorted lines for further processing ( Proprietary algorithm )

--- WebService/ImageJSHelperUtils.js
+++ WebService/ImageJSHelperUtils.js
@@ -66,42 +66,7 @@ exports.buildRecordObjectMapFromImageFile = function (inputFileDataMap, inputFil
         console.log("File Name => " + inputFileName);
         console.log("Text description of starting content  => " + textContentArr[0].description);
 
-        for (var currentText of textContentArr) {
-
-            console.log(currentText.description);
-
-            for (var currentObject of currentText.boundingPoly.vertices) {
-
-                console.log(HelperUtilsModule.returnObjectString(currentObject));
-            }
-        }
-
-        textContentArr = textContentArr.slice(1, textContentArr.length - 1);
-        var minMaxCoordinate = ImageJSHelperUtilsModule.findMinMaxCoordinatesImageContent(textContentArr);
-
-        console.debug("Text description of starting content  => " + textContentArr[0].description +
-            " ,Text description of ending content  => " + textContentArr[textContentArr.length-1].description);
-        console.debug("Min Max Coordinates => xMin : " + minMaxCoordinate.xMin + " ,xMax : " + minMaxCoordinate.xMax +
-            " ,yMin : " + minMaxCoordinate.yMin + " ,yMax : " + minMaxCoordinate.yMax);
-        console.debug("Letter heights => letterMinHeight : " + minMaxCoordinate.letterMinHeight + " ,letterMaxHeight : " +
-            minMaxCoordinate.letterMaxHeight + " ,letterAvgHeight : " + minMaxCoordinate.letterAvgHeight);
-
-        var lineMarkingObjects = ImageJSHelperUtilsModule.segregateImageContentIntoLines(textContentArr, minMaxCoordinate);
-
-        console.debug("Image content segregated into lines :=> ");
-
-        for (var currentObject of lineMarkingObjects) {
-
-            var currentLineText = "New Line : ";
-
-            for (var currentIndex = currentObject.startIndex; currentIndex <= currentObject.endIndex; currentIndex++) {
-
-                currentLineText += textContentArr[currentIndex].description;
-                currentLineText += "   ,";
-            }
-
-            console.debug(currentLineText);
-        }
+        ImageJSHelperUtilsModule.buildRecordObjectsFromImageFilesAndAddToDatabase(textContentArr);
 
     });
 
@@ -112,6 +77,57 @@ exports.buildRecordObjectMapFromImageFile = function (inputFileDataMap, inputFil
     PdfJSHelperUtilsModule.buildRecordObjectMapFromPDFFile(inputFileDataMap, inputFileLocation, inputFileColumnKeys,
         addRecordsToDatabase, addRecordCallbackParams);
     */
+
+}
+
+
+/**
+ * 
+ * @param {Array} textContentArr  : Array of Parsed File contents including Meta_Data
+ * 
+*/
+
+exports.buildRecordObjectsFromImageFilesAndAddToDatabase = function (textContentArr) {
+
+    for (var currentText of textContentArr) {
+
+        console.log(currentText.description);
+
+        for (var currentObject of currentText.boundingPoly.vertices) {
+
+            console.log(HelperUtilsModule.returnObjectString(currentObject));
+        }
+    }
+
+    textContentArr = textContentArr.slice(1, textContentArr.length - 1);
+    var minMaxCoordinate = ImageJSHelperUtilsModule.findMinMaxCoordinatesImageContent(textContentArr);
+
+    console.debug("Text description of starting content  => " + textContentArr[0].description +
+        " ,Text description of ending content  => " + textContentArr[textContentArr.length - 1].description);
+    console.debug("Min Max Coordinates => xMin : " + minMaxCoordinate.xMin + " ,xMax : " + minMaxCoordinate.xMax +
+        " ,yMin : " + minMaxCoordinate.yMin + " ,yMax : " + minMaxCoordinate.yMax);
+    console.debug("Letter heights => letterMinHeight : " + minMaxCoordinate.letterMinHeight + " ,letterMaxHeight : " +
+        minMaxCoordinate.letterMaxHeight + " ,letterAvgHeight : " + minMaxCoordinate.letterAvgHeight);
+
+    var lineMarkingsArray = ImageJSHelperUtilsModule.segregateImageContentIntoLines(textContentArr, minMaxCoordinate);
+
+    console.debug("Image content segregated into lines :=> ");
+
+    for (var currentLineIndexes of lineMarkingsArray) {
+
+        var currentLineText = "New Line : ";
+
+        for (var currentIndex of currentLineIndexes) {
+
+            currentLineText += textContentArr[currentIndex].description;
+            currentLineText += "   ,";
+        }
+
+        console.debug(currentLineText);
+    }
+
+    var sortedImageFileLines = ImageJSHelperUtilsModule.sortImageContentLines(textContentArr, lineMarkingsArray);
+    printImageLines(sortedImageFileLines);
 
 }
 
@@ -195,15 +211,23 @@ exports.findMinMaxCoordinatesImageContent = function (textContentArr) {
 
 exports.segregateImageContentIntoLines = function (textContentArr, minMaxCoordinate) {
 
-    var lineHeightBuffer = 5;
+    var lineHeightBuffer = 8;     // Improve accuracy by taking <mean, min, max> into consideration
     var lineMarkingsObjArr = new Array();
     var bNewLine = true;
     var allowedVerticeMin = 0;
     var allowedVerticeMax = 0;
     var currentLineStartIndex = 0;
     var currentLineEndIndex = 0;
+    var visitedNodes = new Array(textContentArr.length);
+
+    visitedNodes = visitedNodes.fill(false, 0, visitedNodes.length - 1);
 
     for (var currentIndex = 0; currentIndex < textContentArr.length; currentIndex++) {
+
+        if (visitedNodes[currentIndex] == true) {
+
+            continue;
+        }
 
         var currentVertice = ImageJSHelperUtilsModule.retrieveCurrentVericeMinMax(textContentArr, currentIndex);
 
@@ -242,12 +266,36 @@ exports.segregateImageContentIntoLines = function (textContentArr, minMaxCoordin
                     currentIndex--;
                     currentLineEndIndex = currentIndex;
 
-                    var currentLineObj = new Object();
+                    var currentLineIndexes = new Array();
 
-                    currentLineObj.startIndex = currentLineStartIndex;
-                    currentLineObj.endIndex = currentLineEndIndex;
+                    console.debug("currentLineStartIndex : " + currentLineStartIndex + "currentLineEndIndex : " + currentLineEndIndex);
+                    console.debug("visitedNodes[currentLineStartIndex] : " + visitedNodes[currentLineStartIndex].toString() +
+                        "visitedNodes[currentLineEndIndex] : " + visitedNodes[currentLineEndIndex].toString());
 
-                    lineMarkingsObjArr.push(currentLineObj);
+                    for (var currentIdx = currentLineStartIndex; currentIdx <= currentLineEndIndex; currentIdx++) {
+
+                        if (visitedNodes[currentIdx] == false) {
+
+                            currentLineIndexes.push(currentIdx);
+                            visitedNodes[currentIdx] = true;
+                        }
+                    }
+
+                    console.debug("currentLineIndexes after determining clustered vertices : " + currentLineIndexes.toString());
+
+                    var disbursedIndexes = ImageJSHelperUtilsModule.retrieveRemainingVerticesOfCurrentLine(textContentArr, visitedNodes,
+                        allowedVerticeMin, allowedVerticeMax);
+
+                    for (var currentIdx = 0; currentIdx < disbursedIndexes.length; currentIdx++) {
+
+                        currentLineIndexes.push(disbursedIndexes[currentIdx]);
+                        visitedNodes[disbursedIndexes[currentIdx]] = true;
+                    }
+
+                    console.debug("currentLineIndexes after determining scattered vertices : " + currentLineIndexes.toString());
+
+                    lineMarkingsObjArr.push(currentLineIndexes);
+
                 }
 
             }
@@ -289,4 +337,151 @@ exports.retrieveCurrentVericeMinMax = function (textContentArr, currentIndex) {
     return currentVertice;
 }
 
+
+/**
+ * 
+ * @param {Array} textContentArr  : Array of Parsed File contents including Meta_Data
+ * @param {Array} visitedNodes  : Array of indexes with visited nodes marked
+ * @param {Number} allowedVerticeMin  : Allowed Minimum line vertice
+ * @param {Number} allowedVerticeMax  : Allowed Maximum line vertice
+ *
+ * @returns {Array} disbursedVertices  : Array of remaining indexes in current line
+ *
+*/
+
+exports.retrieveRemainingVerticesOfCurrentLine = function (textContentArr, visitedNodes, allowedVerticeMin, allowedVerticeMax) {
+
+    var disbursedVertices = new Array();
+
+    for (var currentIndex = 0; currentIndex < textContentArr.length; currentIndex++) {
+
+        var currentVertice = ImageJSHelperUtilsModule.retrieveCurrentVericeMinMax(textContentArr, currentIndex);
+
+        if (currentVertice.yMin >= allowedVerticeMin && currentVertice.yMax <= allowedVerticeMax && visitedNodes[currentIndex] == false) {
+
+            disbursedVertices.push(currentIndex);
+        }
+    }
+
+    return disbursedVertices;
+
+}
+
+
+/**
+ * 
+ * @param {Array} textContentArr  : Array of Parsed File contents including Meta_Data
+ * @param {Array} lineMarkingsArray  : Array of line index arrays
+ *
+ * @returns {Array} sortedImageFileLines  : Array of sorted lines of the image content
+ *
+*/
+
+exports.sortImageContentLines = function (textContentArr, lineMarkingsArray) {
+
+    var sortedImageFileLines = new Array();
+
+    for (var currentLineIndexArr of lineMarkingsArray) {
+
+        var currentLineContents = new Array();
+
+        for (var currentIndex of currentLineIndexArr) {
+
+            currentLineContents.push(textContentArr[currentIndex]);
+        }
+
+        currentLineContents = sortCurrentLineColumnsWise(currentLineContents);
+        sortedImageFileLines.push(currentLineContents);
+
+    }
+
+    return sortedImageFileLines;
+}
+
+
+/**
+ * 
+ * @param {Array} currentLineContents  : Array of Current Line Contents
+ *
+ * @returns {Array} currentLineContents  : Array of sorted line Contents
+ *
+*/
+
+function sortCurrentLineColumnsWise(currentLineContents) {
+
+    for (var currentIndex = 0; currentIndex < currentLineContents; currentIndex++) {
+
+        currentLineContents = sortVerticesOfCurrentContentColumnWise(currentLineContents, currentIndex);
+    }
+
+    for (var i = 0; i < currentLineContents.length; i++) {
+
+        for (var j = i + 1; j < currentLineContents.length; j++) {
+
+            if ( ((currentLineContents[i].boundingPoly.vertices[0].x + currentLineContents[i].boundingPoly.vertices[1].x) / 2) >
+                 (currentLineContents[j].boundingPoly.vertices[0].x + currentLineContents[j].boundingPoly.vertices[1].x) / 2) {
+
+                var tempHolder = currentLineContents[i];
+                currentLineContents[i] = currentLineContents[j];
+                currentLineContents[j] = tempHolder;
+            }
+        }
+    }
+
+    return currentLineContents;
+}
+
+
+/**
+ * 
+ * @param {Array} textContentArr  : Array of Parsed File contents including Meta_Data
+ * @param {Number} currentIndex  : Current Index of text data for processing
+ *
+ * @returns {Array} textContentArr  : Array of text contents of current lines
+ *
+*/
+
+function sortVerticesOfCurrentContentColumnWise(textContentArr, currentIndex) {
+
+    for (var i = 0; i < textContentArr[currentIndex].boundingPoly.vertices.length; i++) {
+
+        for (var j = i + 1; j < textContentArr[currentIndex].boundingPoly.vertices.length; j++) {
+
+            if (textContentArr[currentIndex].boundingPoly.vertices[i].x > textContentArr[currentIndex].boundingPoly.vertices[j].x) {
+
+                var tempHolder = textContentArr[currentIndex].boundingPoly.vertices[i];
+                textContentArr[currentIndex].boundingPoly.vertices[i] = textContentArr[currentIndex].boundingPoly.vertices[j];
+                textContentArr[currentIndex].boundingPoly.vertices[j] = tempHolder;
+            }
+
+        }
+
+    }
+
+    return textContentArr;
+}
+
+
+/**
+ * 
+ * @param {Array} sortedImageFileLines  : Array of sorted lines of the image content
+ *
+*/
+
+function printImageLines(sortedImageFileLines) {
+
+    for (var currentLine of sortedImageFileLines) {
+
+        var currentLineText = "New Line : ";
+
+        for (var curreLineContent of currentLine) {
+
+            currentLineText += curreLineContent.description;
+            currentLineText += "   ,";
+        }
+
+        console.debug(currentLineText);
+    }
+
+}
 
